6a3c75795803505bfdab394cf4eb9daf295b3798
Peter DiPalma <pdipalm@gmail.com>
2022-09-25 17:27:03 -0400

    Update Deposits.js

--- src/dash/Deposits.js
+++ src/dash/Deposits.js
@@ -10,16 +10,16 @@ function preventDefault(event) {
 export default function Deposits() {
   return (
     <React.Fragment>
-      <Title>Recent Deposits</Title>
+      <Title>Recent Progress</Title>
       <Typography component="p" variant="h4">
-        $3,024.00
+        +0
       </Typography>
       <Typography color="text.secondary" sx={{ flex: 1 }}>
-        on 15 March, 2019
+        on January 1st, 1990
       </Typography>
       <div>
         <Link color="primary" href="#" onClick={preventDefault}>
-          View balance
+          View details
         </Link>
       </div>
     </React.Fragment>
