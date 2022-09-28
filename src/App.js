bba4dcab80db98de99a26ca56841cfa9d2766e79
joeysaline <jsaline@albany.edu>
2022-09-28 14:24:12 -0400

    Update to fix copmile error
    not solved

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState } from 'react';
 import Dashboard from './dash/Dashboard';
 import SignUp from './user-auth/SignUp'
+import { AuthProvider } from './user-auth/contexts/AuthContexts'
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
